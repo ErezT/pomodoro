5150f00734c24d6b60fa9d2118b89da6c8c21ca9
Erez Tavor <erezpitke@gmail.com>
2017-08-13 22:55:49 +0300

    CSS changes to buttons

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
 
-// var now = moment().minutes(00).seconds(05);
 var endTime = moment().minutes(00).seconds(00);
 var onScreenClock = document.getElementById('time');
 var intervalID;
@@ -8,13 +7,11 @@ var now = moment().minutes(userInput).seconds(00);
 
 
 function showTime() {
-    if(now.second() == 00 && now.minute() == 00) {
+    if (now.second() == 00 && now.minute() == 00) {
         console.log(now.second() + ":" + now.minute());
         stopper();
         console.log("stopped");
     } else {
-        // console.log(now.second());
-        // console.log(now.minutes());
         console.log(now.subtract(1, 'seconds').format("mm:ss"));
         onScreenClock.innerHTML = now.minute() + ":" + now.second();
     }
@@ -29,15 +26,15 @@ function stopper() {
 }
 
 function reseter() {
-    onScreenClock.innerHTML = now.set('minute',25).set('seconds', 00).format('mm:ss');  
+    onScreenClock.innerHTML = now.set('minute', 25).set('seconds', 00).format('mm:ss');
 }
 
 function addTime() {
-    now.add(1, 'minutes');   
-    onScreenClock.innerHTML = now.minutes() + ":" + now.second();    
+    now.add(1, 'minutes');
+    onScreenClock.innerHTML = now.minutes() + ":" + now.second();
 }
 
 function subtractTime() {
     now.subtract(1, 'minutes');
-    onScreenClock.innerHTML = now.minutes() + ":" + now.second();    
+    onScreenClock.innerHTML = now.minutes() + ":" + now.second();
 }
